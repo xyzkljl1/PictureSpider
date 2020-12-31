64adbc798a89e65167fccf7e66e0a452a5cec585
xyzkljl1 <xyzkljl1@yahoo.com>
2021-01-01 00:59:51 +0800

    添加系统托盘 修改readme

--- PixivAss/Control/MainWindow.cs
+++ PixivAss/Control/MainWindow.cs
@@ -25,6 +25,7 @@ namespace PixivAss
             queueComboBox.SetClient(pixiv_client);
             //UI
             //parent为主窗口的话，透明时会透出主窗口的背景，而主窗口背景不能设为透明，所以需要更改parent
+            SystemTrayIcon.Icon = Properties.Resources.baseIcon;
             PageLabel.Parent = this.MainExplorer;
             PageLabel.Location = new System.Drawing.Point(PageLabel.Location.X - MainExplorer.Location.X, PageLabel.Location.Y - MainExplorer.Location.Y);
             NextButton.Parent = this.MainExplorer;
@@ -47,6 +48,8 @@ namespace PixivAss
             PlayButton.Click += (object sender, EventArgs e) => MainExplorer.Play();
             RandomSlideCheckBox.Click += (object sender, EventArgs e) => MainExplorer.random_slide=RandomSlideCheckBox.Checked;
             InitButton.Click += (object sender, EventArgs e) =>pixiv_client.InitTask().Wait();
+            SystemTrayIcon.DoubleClick += (object sender, EventArgs e) => this.Visible = !this.Visible;
+            ExitAction.Click+= (object sender, EventArgs e) => this.Close(); 
             //绑定属性
             PageLabel.DataBindings.Add(new Binding("Text", MainExplorer.GetBindHandle<string>("IndexText"), "Content"));
             DescBrowser.DataBindings.Add(new Binding("DocumentText", MainExplorer.GetBindHandle<string>("DescText"), "Content"));
@@ -59,6 +62,12 @@ namespace PixivAss
             DataBindings.Add(new Binding("Text", pixiv_client.GetBindHandle<string>("VerifyState"), "Content"));
 
             //onListCheckBoxClick(null,null);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
         }
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
@@ -75,9 +84,14 @@ namespace PixivAss
         }
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-            PixivAss.BindHandleProvider bindHandleProvider2 = new PixivAss.BindHandleProvider();
+            PixivAss.BindHandleProvider bindHandleProvider1 = new PixivAss.BindHandleProvider();
+            this.InitButton = new System.Windows.Forms.Button();
+            this.queueComboBox = new PixivAss.QueueComboBox();
             this.RandomSlideCheckBox = new System.Windows.Forms.CheckBox();
+            this.AuthorBox = new PixivAss.AuthorBox();
+            this.TagBox = new PixivAss.TagBox();
             this.DescBrowser = new System.Windows.Forms.WebBrowser();
             this.PlayButton = new System.Windows.Forms.Button();
             this.PageLabel = new System.Windows.Forms.Label();
@@ -87,14 +101,14 @@ namespace PixivAss
             this.OpenLocalLabel = new System.Windows.Forms.LinkLabel();
             this.SwitchBookmarkButton = new System.Windows.Forms.Label();
             this.BookmarkPageLabel = new System.Windows.Forms.Label();
-            this.queueComboBox = new PixivAss.QueueComboBox();
-            this.AuthorBox = new PixivAss.AuthorBox();
-            this.TagBox = new PixivAss.TagBox();
             this.MainExplorer = new PixivAss.Explorer();
-            this.InitButton = new System.Windows.Forms.Button();
+            this.SystemTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SystemTrayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ExitAction = new System.Windows.Forms.ToolStripMenuItem();
             flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.MainExplorer)).BeginInit();
+            this.SystemTrayMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -114,6 +128,27 @@ namespace PixivAss
             flowLayoutPanel1.Size = new System.Drawing.Size(164, 469);
             flowLayoutPanel1.TabIndex = 14;
             // 
+            // InitButton
+            // 
+            this.InitButton.Location = new System.Drawing.Point(3, 3);
+            this.InitButton.Name = "InitButton";
+            this.InitButton.Size = new System.Drawing.Size(158, 74);
+            this.InitButton.TabIndex = 20;
+            this.InitButton.Text = "RunInitTask";
+            this.InitButton.UseVisualStyleBackColor = true;
+            this.InitButton.Visible = false;
+            // 
+            // queueComboBox
+            // 
+            this.queueComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.queueComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.queueComboBox.FormattingEnabled = true;
+            this.queueComboBox.Location = new System.Drawing.Point(3, 83);
+            this.queueComboBox.Name = "queueComboBox";
+            this.queueComboBox.Size = new System.Drawing.Size(101, 20);
+            this.queueComboBox.TabIndex = 19;
+            // 
             // RandomSlideCheckBox
             // 
             this.RandomSlideCheckBox.Location = new System.Drawing.Point(107, 80);
@@ -124,6 +159,30 @@ namespace PixivAss
             this.RandomSlideCheckBox.Text = "Rock";
             this.RandomSlideCheckBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.RandomSlideCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // AuthorBox
+            // 
+            this.AuthorBox.Location = new System.Drawing.Point(3, 109);
+            this.AuthorBox.Name = "AuthorBox";
+            this.AuthorBox.Size = new System.Drawing.Size(154, 20);
+            this.AuthorBox.TabIndex = 12;
+            this.AuthorBox.UserId = 0;
+            // 
+            // TagBox
+            // 
+            this.TagBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TagBox.AutoScroll = true;
+            this.TagBox.BackColor = System.Drawing.Color.Transparent;
+            this.TagBox.CausesValidation = false;
+            this.TagBox.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.TagBox.Location = new System.Drawing.Point(0, 132);
+            this.TagBox.Margin = new System.Windows.Forms.Padding(0);
+            this.TagBox.Name = "TagBox";
+            this.TagBox.Size = new System.Drawing.Size(161, 132);
+            this.TagBox.TabIndex = 9;
+            this.TagBox.Tags = null;
+            this.TagBox.WrapContents = false;
             // 
             // DescBrowser
             // 
@@ -234,41 +293,6 @@ namespace PixivAss
             this.BookmarkPageLabel.Text = "X";
             this.BookmarkPageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // queueComboBox
-            // 
-            this.queueComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.queueComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.queueComboBox.FormattingEnabled = true;
-            this.queueComboBox.Location = new System.Drawing.Point(3, 83);
-            this.queueComboBox.Name = "queueComboBox";
-            this.queueComboBox.Size = new System.Drawing.Size(101, 20);
-            this.queueComboBox.TabIndex = 19;
-            // 
-            // AuthorBox
-            // 
-            this.AuthorBox.Location = new System.Drawing.Point(3, 109);
-            this.AuthorBox.Name = "AuthorBox";
-            this.AuthorBox.Size = new System.Drawing.Size(154, 20);
-            this.AuthorBox.TabIndex = 12;
-            this.AuthorBox.UserId = 0;
-            // 
-            // TagBox
-            // 
-            this.TagBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.TagBox.AutoScroll = true;
-            this.TagBox.BackColor = System.Drawing.Color.Transparent;
-            this.TagBox.CausesValidation = false;
-            this.TagBox.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.TagBox.Location = new System.Drawing.Point(0, 132);
-            this.TagBox.Margin = new System.Windows.Forms.Padding(0);
-            this.TagBox.Name = "TagBox";
-            this.TagBox.Size = new System.Drawing.Size(161, 132);
-            this.TagBox.TabIndex = 9;
-            this.TagBox.Tags = null;
-            this.TagBox.WrapContents = false;
-            // 
             // MainExplorer
             // 
             this.MainExplorer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -276,21 +300,31 @@ namespace PixivAss
             | System.Windows.Forms.AnchorStyles.Right)));
             this.MainExplorer.Location = new System.Drawing.Point(182, 12);
             this.MainExplorer.Name = "MainExplorer";
-            this.MainExplorer.provider = bindHandleProvider2;
+            this.MainExplorer.provider = bindHandleProvider1;
             this.MainExplorer.Size = new System.Drawing.Size(850, 582);
             this.MainExplorer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.MainExplorer.TabIndex = 2;
             this.MainExplorer.TabStop = false;
             // 
-            // InitButton
+            // SystemTrayIcon
             // 
-            this.InitButton.Location = new System.Drawing.Point(3, 3);
-            this.InitButton.Name = "InitButton";
-            this.InitButton.Size = new System.Drawing.Size(158, 74);
-            this.InitButton.TabIndex = 20;
-            this.InitButton.Text = "RunInitTask";
-            this.InitButton.UseVisualStyleBackColor = true;
-            this.InitButton.Visible = false;
+            this.SystemTrayIcon.ContextMenuStrip = this.SystemTrayMenu;
+            this.SystemTrayIcon.Text = "PixivAss";
+            this.SystemTrayIcon.Visible = true;
+            // 
+            // SystemTrayMenu
+            // 
+            this.SystemTrayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitAction});
+            this.SystemTrayMenu.Name = "SystemTrayMenu";
+            this.SystemTrayMenu.Size = new System.Drawing.Size(181, 48);
+            this.SystemTrayMenu.Opening += new System.ComponentModel.CancelEventHandler(this.SystemTrayMenu_Opening);
+            // 
+            // ExitAction
+            // 
+            this.ExitAction.Name = "ExitAction";
+            this.ExitAction.Size = new System.Drawing.Size(180, 22);
+            this.ExitAction.Text = "退出";
             // 
             // MainWindow
             // 
@@ -307,8 +341,10 @@ namespace PixivAss
             this.Controls.Add(this.PlayButton);
             this.KeyPreview = true;
             this.Name = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
             flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.MainExplorer)).EndInit();
+            this.SystemTrayMenu.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -347,5 +383,20 @@ namespace PixivAss
         private Label BookmarkPageLabel;
         private CheckBox RandomSlideCheckBox;
         private Button InitButton;
+        private NotifyIcon SystemTrayIcon;
+        private System.ComponentModel.IContainer components;
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+        }
+
+        private ContextMenuStrip SystemTrayMenu;
+
+        private void SystemTrayMenu_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private ToolStripMenuItem ExitAction;
     }
 }
